1fee258d2a40bcde38838cc85db8f48952665a4c
ssch71 <s01093450498@gmail.com>
2026-07-21 14:13:00 +0900

    Delete target/a53x directory

--- target/a53x/patches/midas/customize.sh
+++
@@ -1,9 +0,0 @@
-LOG_STEP_IN "- Adding stock PhotoRemasterService"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/PhotoRemasterService" 0 0 755 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libmidas_core.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libmidas_DNNInterface.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-LOG_STEP_OUT
-
-LOG_STEP_IN "- Fixing MIDAS model detection"
-sed -i "s/ro.product.device/ro.product.vendor.device/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
-LOG_STEP_OUT
