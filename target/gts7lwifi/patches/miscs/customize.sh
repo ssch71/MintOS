1ce4339c1db84fda2830524a0e8a1526d870dc97
ssch71 <s01093450498@gmail.com>
2026-07-21 13:05:25 +0900

    miscs/customize.sh 만들기

---
+++ target/gts7lwifi/patches/miscs/customize.sh
@@ -0,0 +1,13 @@
+LOG_STEP_IN "- Fixing up /product/etc/build.prop"
+sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Enabling adaptive FPS"
+SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "true"
+sed -i \
+    "/use_content_detection/a ro.surface_flinger.set_idle_timer_ms=3000\nro.surface_flinger.set_touch_timer_ms=500\nro.surface_flinger.set_display_power_timer_ms=1000" \
+    "$WORK_DIR/vendor/default.prop"
+LOG_STEP_OUT
